eb0e742905369fd9dc8fae56ee54193a092f24fe
P3t3rtm <33187689+P3t3rtm@users.noreply.github.com>
2022-06-13 20:19:01 -0600

    venv

---
+++ app.py
@@ -0,0 +1,12 @@
+#import serial
+#import io
+
+print("Hello World")
+
+# ser = serial.Serial('COM9', 921600, timeout=0, 
+#     parity=serial.PARITY_EVEN, rtscts=1)
+
+# print(ser.name)         # check which port was really used
+# print("Hello World2")
+
+# ser.close()             # close port
